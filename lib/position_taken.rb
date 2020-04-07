88bffd7ce4da8c91910b6e87b1144cf9712ca7c3
Zoey Chesny <z.chesny@gmail.com>
2020-04-07 21:41:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == ""
+  if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
     return true
